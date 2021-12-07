3b96efe54b076b9dd95fea572023af674ede1024
Pawar97 <91775938+Pawar97@users.noreply.github.com>
2021-12-07 13:18:16 +0530

    initial commit

---
+++ RemoveArrayeElement.java
@@ -0,0 +1,13 @@
+class PrintGrid {
+
+	public static int[] removeTheElement(int[] arr, int index)
+	{
+
+		System.out.println("-------------");
+                System.out.println("-------------");
+                System.out.println("-------------");
+                System.out.println("-------------");
+                System.out.println("-------------");
+                System.out.println("-------------");
+}
+}
