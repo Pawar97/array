a17919c7530f6ad9cef29244ef8ebf4439e70c02
Pawar97 <91775938+Pawar97@users.noreply.github.com>
2021-12-08 11:11:07 +0530

    Update PrintGrid.java

--- PrintGrid.java
+++ PrintGrid.java
@@ -3,11 +3,11 @@ class PrintGrid {
 	public static void main(String[] args)
 	{
 
-		System.out.println("-------------");
-                System.out.println("-------------");
-                System.out.println("-------------");
-                System.out.println("-------------");
-                System.out.println("-------------");
-                System.out.println("-------------");
+		for(int i=0;i<10;i++){
+	        for(int j=0;j<10;i++){
+			System.out.print("-");
+		}
+			]
+				System.out.println();
 }
 }
