7eaf0260885543f63503abcea8ddd49bc7312a61
Pawar97 <91775938+Pawar97@users.noreply.github.com>
2021-12-16 13:37:11 +0530

    Add files via upload

---
+++ StringAssignment/IndexOfChar.java
@@ -0,0 +1,40 @@
+
+public class IndexOfChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1="The lion eats catches the dear";
+int a=s1.indexOf("a",0);
+int b=s1.indexOf("b",0);
+int c=s1.indexOf("c",0);
+int d=s1.indexOf("d",0);
+int e=s1.indexOf("e",0);
+int f=s1.indexOf("f",0);
+int g=s1.indexOf("g",0);
+int h=s1.indexOf("h",0);
+int i=s1.indexOf("i",0);
+int j=s1.indexOf("j",0);
+int k=s1.indexOf("k",0);
+int l=s1.indexOf("l",0);
+int m=s1.indexOf("m",0);
+int n=s1.indexOf("n",0);
+int o=s1.indexOf("o",0);
+int p=s1.indexOf("p",0);
+int q=s1.indexOf("q",0);
+int r=s1.indexOf("r",0);
+int s=s1.indexOf("s",0);
+int t=s1.indexOf("t",0);
+int u=s1.indexOf("u",0);
+int v=s1.indexOf("v",0);
+int w=s1.indexOf("w",0);
+int x=s1.indexOf("x",0);
+int y=s1.indexOf("y",0);
+int z=s1.indexOf("z",0);
+
+System.out.println("abcdefghijklmnopqrstuvwxyz");
+System.out.println("--------------------------------------------------------------------------------------------------");
+System.out.println(a+""+b+""+c+""+d+""+e+""+f+""+g+""+h+""+i+""+j+""+k+""+l+""+m+""+n+""+o+""+p+""+q+""+r+""+s+""+t+""+
+u+""+v+""+w+""+x+""+y+""+z);
+	}
+
+}
---
+++ StringAssignment/Substring.java
@@ -0,0 +1,13 @@
+
+public class Substring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="The quick brown fox jumps over the lazy dog";
+		System.out.println("Orignal string:"+s1);
+		System.out.println("Substring:"+s1.substring(10,25));
+		
+
+	}
+
+}
